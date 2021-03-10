1d80f3a046faeef9aaea3ae938430a235ecb1d5c
unknown <s11810234@student.unklab.ac.id>
2021-03-10 22:18:37 +0800

    Class, Inheritance

---
+++ Exercises-03/index.js
@@ -0,0 +1,42 @@
+//Class
+class orang {
+    constructor(nama, umur) 
+    {this.nama = nama; 
+        this.umur = umur;
+    }
+
+    bekerja() {
+        return `${this.nama} sedang bekerja seperti biasa`;
+    }
+}
+
+let user1 = new orang("Ricky Pasulatan", 20);
+console.log(user1.bekerja());
+
+//Inheritance
+class Orang {
+    constructor(nama, umur) {
+        this.nama = nama; 
+        this.umur = umur;
+    }
+    tidur() {
+        return `${this.nama} sedang tidur`;
+    }
+    makan() {
+        return `${this.nama} sedang makan`;
+    }
+}
+
+class Pelajar extends Orang {
+    constructor(nama, umur, namaSekolah) {
+        super(nama, umur); 
+        this.namaSekolah=namaSekolah;
+    }
+
+    belajar() {
+        return `${this.nama} belajar di ${this.namaSekolah}`;
+    }
+}
+
+let user = new Pelajar("John", 17, "Unklab");
+console.log(user)
